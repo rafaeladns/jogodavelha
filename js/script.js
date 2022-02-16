a99d45d3203a75ce3cbd58bd8d923e3e2cf60ecc
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-02-16 01:50:06 -0300

    CSS do jogo parcialmente finalizado e js iniciado

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Selecionando x e o//
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+
+//Selecionando box//
+let boxes = document.querySelectorAll(".box");
+
+let buttons = document.querySelectorAll("#button-container button");
+
+let messageContainer = document.querySelector("#message")
